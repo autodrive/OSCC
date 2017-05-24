6e6ab9d0b5db25bfd4b18670283ccb61cb9a87ad
Katie Cleary <kcleary@polysync.io>
2017-05-23 18:27:57 -0700

    Update for style guide Prior to this commit, I forgot to run rust fmt. This commit has the tests run through rustfmt for style guide compatibility.

--- platforms/kia_soul/firmware/steering/tests/property/src/tests.rs
+++ platforms/kia_soul/firmware/steering/tests/property/src/tests.rs
@@ -96,7 +96,7 @@ impl Arbitrary for can_frame_s {
                    u8::arbitrary(g),
                    u8::arbitrary(g),
                    u8::arbitrary(g),
-                   u8::arbitrary(g)]
+                   u8::arbitrary(g)],
         }
     }
 }
@@ -107,7 +107,7 @@ impl Arbitrary for oscc_report_chassis_state_2_data_s {
             wheel_speed_front_left: i16::arbitrary(g),
             wheel_speed_front_right: i16::arbitrary(g),
             wheel_speed_rear_left: i16::arbitrary(g),
-            wheel_speed_rear_right: i16::arbitrary(g)
+            wheel_speed_rear_right: i16::arbitrary(g),
         }
     }
 }
@@ -133,7 +133,7 @@ impl Arbitrary for pid_s {
             prev_input: f32::arbitrary(g),
             int_error: f32::arbitrary(g),
             control: f32::arbitrary(g),
-            prev_steering_angle: f32::arbitrary(g)
+            prev_steering_angle: f32::arbitrary(g),
         }
     }
 }
@@ -575,14 +575,12 @@ fn prop_check_rx_chassis_2(chassis_msg: oscc_report_chassis_state_2_s) -> TestRe
 
         check_for_incoming_message();
 
-        let wheel_speed_avg: f32 = (
-            chassis_msg.data.wheel_speed_front_left + 
-            chassis_msg.data.wheel_speed_front_right + 
-            chassis_msg.data.wheel_speed_rear_left + 
-            chassis_msg.data.wheel_speed_rear_right ) as f32
-            / 4.0;
+        let wheel_speed_avg: f32 =
+            (chassis_msg.data.wheel_speed_front_left + chassis_msg.data.wheel_speed_front_right +
+             chassis_msg.data.wheel_speed_rear_left +
+             chassis_msg.data.wheel_speed_rear_right) as f32 / 4.0;
 
-        let vehicle_speed_kmh: i16 =  ((wheel_speed_avg / 128.0) * 160.9 ) as i16;
+        let vehicle_speed_kmh: i16 = ((wheel_speed_avg / 128.0) * 160.9) as i16;
 
         TestResult::from_bool(g_steering_control_state.vehicle_speed as i16 ==
                               vehicle_speed_kmh as i16)
@@ -638,10 +636,6 @@ fn prop_check_torque_constraint(vehicle_speed: f32,
                                        TORQUE_MAX_IN_NEWTON_METERS) +
                                       SPOOF_HIGH_SIGNAL_CALIBRATION_CURVE_OFFSET);
 
-        let min_high_for_kmh_step = (STEPS_PER_VOLT * 
-            ((SPOOF_HIGH_SIGNAL_CALIBRATION_CURVE_SCALAR * control_value_max)
-            + SPOOF_HIGH_SIGNAL_CALIBRATION_CURVE_OFFSET)) as u16;
-        
         // need to reverse signs, since SPOOF_HIGH_CURVE_SCALAR is negative
         let max_high_for_kmh_step = STEPS_PER_VOLT *
                                      ((SPOOF_HIGH_SIGNAL_CALIBRATION_CURVE_SCALAR *
